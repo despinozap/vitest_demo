1bd832fda4567584372afd456916eeea7cd792a7
David Espinoza <despinozap.dev@gmail.com>
2023-01-11 18:45:09 -0300

    test: Test added for sum numbers function

--- src/services/utilsService.test.js
+++ src/services/utilsService.test.js
@@ -2,10 +2,10 @@ import { describe, it, expect } from 'vitest';
 import { sumNumbers } from './utilsService';
 
 describe(
-  'Utils service',
+  'sumNumbers',
   () => {
     it(
-      'sumNumbers sums numbers successfully',
+      'returns sum of numbers successfully',
       () => {
         // Sum is 55
         const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -16,13 +16,24 @@ describe(
     );
 
     it(
-      'sumNumbers returns null when invalid parameter',
+      'returns null when invalid parameter',
       () => {
         // Sum is 55
         const numbers = 'hellostring';
         const result = sumNumbers(numbers);
         
         expect(result).toBe(null);
+      }
+    );
+
+    it(
+      'returns 0 when array is empty',
+      () => {
+        // Sum is 0
+        const numbers = [];
+        const result = sumNumbers(numbers);
+        
+        expect(result).toBe(0);
       }
     );
   }
